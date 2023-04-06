091c8cc50ca039d61934fe884cb9f6d8710757b1
Andrey9595 <anb95.95@mail.ru>
2023-04-06 19:07:23 +0300

    add news

--- app/src/main/java/ru/anb/sportapp/ui/ChoiceFragment.kt
+++ app/src/main/java/ru/anb/sportapp/ui/ChoiceFragment.kt
@@ -5,14 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
 import ru.anb.sportapp.R
+import ru.anb.sportapp.databinding.FragmentChoiceBinding
 
 class ChoiceFragment : Fragment() {
+
+    private var _binding: FragmentChoiceBinding? = null
+    private val mBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_choice, container, false)
+        _binding = FragmentChoiceBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getNews()
+    }
+
+    private fun getNews() {
+        mBinding.btnNews.setOnClickListener {
+            val action = ChoiceFragmentDirections.actionChoiceFragmentToNewsFragment()
+            NavHostFragment.findNavController(this).navigate(action)
+        }
     }
 }
